3d9c3cda73e93eb973319cf73455fb1a164cf20a
Matheus Miranda <matheusmiranda.batista@gmail.com>
2026-01-15 16:49:14 -0300

    First Commit

---
+++ src/main/java/br/com/controleresidencial/ControleResidencialApplication.java
@@ -0,0 +1,13 @@
+package br.com.controleresidencial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleResidencialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleResidencialApplication.class, args);
+	}
+
+}
